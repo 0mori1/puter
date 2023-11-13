c34640e9d0fc2aeb5f0ba72d819c06393e9191cf
0mori1 <117737361+0mori1@users.noreply.github.com>
2023-11-13 11:48:00 +0900

    Update putercode.lua

--- code/putercode.lua
+++ code/putercode.lua
@@ -663,10 +663,13 @@ local success, errorcode = pcall(function()
 				return windowframe, closebutton, titlebar, isActive
 			end;
 			new = function(app, name, windowProperties)
-				local window, closebutton, titlebar = puter.CreateWindow(windowProperties.xSize, windowProperties.ySize, name, windowProperties.backgroundColor, windowProperties.titlebarColor, windowProperties.textColor, windowProperties, windowProperties.xPos, windowProperties.yPos)
-				newCoroutine(function()
+				local window, closebutton, titlebar = puter.CreateWindow(windowProperties.xSize, windowProperties.ySize, name, windowProperties.backgroundColor, windowProperties.titlebarColor, windowProperties.textColor, windowProperties.xPos, windowProperties.yPos)
+				local PID = newCoroutine(function()
 					app(puter, puterutils, {["window"] = window, ["closebutton"] = closebutton, ["titlebar"] = titlebar})
 				end, name)
+				closebutton.MouseButton1Click:Connect(function()
+					closeCoroutine(PID)
+				end)
 			end;
 		})
 		rom:Write("puterutils", {
